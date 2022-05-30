2a907e756d9211efbd836396ed94b63ab009a948
Ludwigvsch <lvschoenfeldt@gmx.de>
2022-05-29 21:31:55 -0700

    Initial Commit

---
+++ Project5/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Project5
+//
+//  Created by Ludwig von Schönfeldt  on 29.05.22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
