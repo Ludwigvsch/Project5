2bbd52685404b218cda0ebda6668bce4fa3a7923
Ludwigvsch <lvschoenfeldt@gmx.de>
2022-06-04 13:23:51 -0700

    finishing project

---
+++ Project5/studentinfo.h
@@ -0,0 +1,10 @@
+#ifndef STUDENTINFO_H
+#define STUDENTINFO_H
+#include<string>
+
+namespace StudentInfo {
+    std::string name() { return "Ludwig von Schoenfeldt"; } //Add your name, no special characters
+    std::string id() { return "1871721"; } //Add your id, no special characters
+};
+
+#endif
